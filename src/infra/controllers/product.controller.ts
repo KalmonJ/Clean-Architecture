e80175e03332d07f8a44cd692d75cd29574b2045
kalmonJ <kalmonkk69@gmail.com>
2023-04-01 22:35:51 -0300

    feat: update methods product controller

--- src/infra/controllers/product.controller.ts
+++ src/infra/controllers/product.controller.ts
@@ -1,11 +1,15 @@
 import { CreateProductUseCase } from "../../application/usecases/create-product.use-case";
+import { GetAllProductsByCategoryUseCase } from "../../application/usecases/get-all-products-by-category.use-case";
+import { GetAllProductsOfTheWeekUseCase } from "../../application/usecases/get-all-products-of-the-week.use-case";
 import { UpdateProductUseCase } from "../../application/usecases/update-product.use-case";
 import { Request, Response } from "express";
 
 export class UserController {
   constructor(
     private createProductUseCase: CreateProductUseCase,
-    private updateProductUseCase: UpdateProductUseCase
+    private updateProductUseCase: UpdateProductUseCase,
+    private getAllProductsByCategoryUseCase: GetAllProductsByCategoryUseCase,
+    private getAllProductsOfTheWeekUseCase: GetAllProductsOfTheWeekUseCase
   ) {}
 
   async createProduct(req: Request, res: Response) {
@@ -16,6 +20,30 @@ export class UserController {
         throw new Error(Product.error);
       }
       return res.status(201).json(Product);
+    } catch (error: any) {
+      return res.status(400).json({ message: error.message });
+    }
+  }
+
+  async getAllProductsByCategory(req: Request, res: Response) {
+    try {
+      const allProductsByCategory =
+        await this.getAllProductsByCategoryUseCase.execute({
+          category: req.body,
+        });
+
+      return res.status(200).json(allProductsByCategory);
+    } catch (error: any) {
+      return res.status(400).json({ message: error.message });
+    }
+  }
+
+  async getAllProductsOfTheWeek(_: Request, res: Response) {
+    try {
+      const productsOfTheWeek =
+        await this.getAllProductsOfTheWeekUseCase.execute();
+
+      return res.status(200).json(productsOfTheWeek);
     } catch (error: any) {
       return res.status(400).json({ message: error.message });
     }
